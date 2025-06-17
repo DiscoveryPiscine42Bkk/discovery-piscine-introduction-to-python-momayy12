ea0cd9f565321b26dedb4082b8dc128b4efa937c
Methisa <metisa4878@gmail.com>
2025-06-17 01:58:09 +0000

    งาน2

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Methisa"
+last_name = "Teepaadisai"
+print(first_name,last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Methisa"
+last_name = "Teepaadisai"
+Whole_name = first_name + " " + last_name
+print(Whole_name) 
