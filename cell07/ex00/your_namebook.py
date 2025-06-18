dc792bc047bbe4b158532fcfef937826fbc8a62c
Methisa <metisa4878@gmail.com>
2025-06-18 08:01:05 +0000

    cell07ex00

---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,15 @@
+def array_of_names(name_dict):
+    full_name = []
+    for first, last in name_dict.items():
+        full_name = f"{first.capitalize()} {last.capitalize()}"
+        full_name.append(full_name)
+    return full_names
+
+persons = {
+    "sawittcha": "jiamthanunkul" , 
+    "chutinun": "yungmak",
+    "geno": "pum",
+    "pum": "geno"
+} 
+
+print(array_of_names(persons))
